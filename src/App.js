7687e4f5f6aa7f85c8959ebb30e68cf9fdecfb0a
Alex Goater <agoater44@icloud.com>
2025-09-25 11:55:55 +0000

    Fixed Square Value Overrides

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
@@ -22,6 +25,7 @@ export default function Board() {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
+
   return (
     <>
       <div className="board-row">
