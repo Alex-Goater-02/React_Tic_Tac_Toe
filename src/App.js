8b74d6fe36e5f570447410741e04e1eff12fb98f
Alex Goater <agoater44@icloud.com>
2025-09-25 12:30:31 +0000

    Fully Working Game

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function Board({ xIsNext, squares, onPlay }) {
   if (winner) {
     status = "Winner: " + winner;
   } else if (!squares.includes(null)) {
-    status = "It's a draw!";
+    status = "It's a Draw!";
   } else {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
@@ -55,16 +55,21 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
 
-  function jumpTo(nextMove) {}
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
